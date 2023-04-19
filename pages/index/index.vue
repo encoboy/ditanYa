d0dacebb6386134c0d9e63e7636d42d8cd443607
糖醋加冰 <8083156+luoban222@user.noreply.gitee.com>
2023-04-19 09:19:23 +0800

    创建项目

---
+++ pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="container">
+		
+		<view class="intro">本项目</view>
+	
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less">
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+		.intro{
+			color: red;
+		}
+	}
+</style>
